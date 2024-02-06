55526d57abc8f7aa609a959d558a920f3ac46687
atao <87337334@qq.com>
2024-02-06 14:01:41 +0800

    fix markdown'js invalid

---
+++ dist/_astro/SiderbarHome.astro_astro_type_script_index_0_lang.vsUn6Q78.js
@@ -0,0 +1,1 @@
+const t=document.querySelectorAll(".archive-month");t.forEach(e=>{e.addEventListener("click",()=>{const n=e.nextElementSibling;n.style.display=n.style.display==="none"?"block":"none"})});
